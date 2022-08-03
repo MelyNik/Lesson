eeabb8176fad6c1611e4411e8eb692fa76024e3f
Nikita <number_nik@mail.ru>
2022-08-03 15:43:16 +0300

    Добавил комментарии

---
+++ Exe/Example001_HELLOCON/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!"); 
+// Console - позволяет работать с окном терминала.
+// WtiteLine - позволяет вывести, что то в окно терминала, в данном случае "Hello, World!" 
