37d265cf24d50c5b104fcc70783857072eaebd4b
Nikita <number_nik@mail.ru>
2022-08-04 14:34:47 +0300

    Задача 5 особенное приветствие Маши с разными регистрами и обычное приветствие остальных пользователей

---
+++ Exe/Example005_HelloFriends_and_Macha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if ( username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else 
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
