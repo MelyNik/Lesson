7c1cb6ddec67f9952799814004447da439b9196a
Nikita <number_nik@mail.ru>
2022-09-16 16:48:31 +0300

    Дополнительная лекция рекурсии

--- Exe/Example18_RekursyaContinuation/Program.cs
+++ Exe/Example18_RekursyaContinuation/Program.cs
@@ -16,10 +16,8 @@
 //    return result;
 //}
 //string result = NumbersFor(1,5);
-//Console.WriteLine(result);
-
-// С рекурсией:
-
+//Console.WriteLine(result)//
+// С рекурсией//
 //string NumbersRec(int a, int b)
 //{
 //    if(a+1<=b)
@@ -181,13 +179,11 @@
 //    {
 //        return NumbersRecFOR(a, n -1) * a;
 //    }
-//}
-
+//
 // Задача 6. Перебор слов.
 // В некотором машинном алфавите имеются четыре буквы "а", "и", "с" и "в".
-// Покажите все слова, состоящие из Т букв, которые можно построить из букв этого фалфавита.
-
-// Однобуквенные слова:
+// Покажите все слова, состоящие из Т букв, которые можно построить из букв этого фалфавита
+// Однобуквенные слова
 
 //char[] s = { 'а', 'и', 'с', 'в' };
 //int count = s.Length;
@@ -195,11 +191,11 @@
 //for (int i = 0; i < count; i++)
 //{
 //    Console.WriteLine($"{n++,-5}{s[i]}");
-//}
+//
 
-// Пяти буквенные (потому, что цикл в цикле в цикле в цикле в цикле).
+// Пяти буквенные (потому, что цикл в цикле в цикле в цикле в цикле)
 
-// Без рекурсии:
+// Без рекурсии
 
 //char[] s = { 'а', 'и', 'с', 'в' };
 //int count = s.Length;
@@ -219,11 +215,8 @@
 //            }
 //        }
 //    }
-//}
-
-// С рекурсией:
-
-
+//
+// С рекурсией
 //int n = 1;
 //void FindWords(string alphabet, char[] word, int length = 0)
 //{
@@ -240,27 +233,100 @@
 //}
 //
 //FindWords("аисв", new char[2]);
+/// Что бы пользоваться нужной деректорией, а именно выводить какую либо информацию папок и папок в папках.
+//void CatalogInfo(string path, string indent = "")
+//{
+//    DirectoryInfo catalog = new DirectoryInfo(path);
+//
+//    DirectoryInfo[] catalogs = catalog.GetDirectories();
+//    for (int i = 0; i < catalogs.Length; i++)
+//    {
+//        Console.WriteLine($"{indent}{catalogs[i].Name}");
+//        CatalogInfo(catalogs[i].FullName, indent + " ");
+//    }
+//
+//    FileInfo[] files = catalog.GetFiles();
+//
+//    for (int i = 0; i < files.Length; i++)
+//    {
+//        Console.WriteLine($"{indent}{files[i]}.Name");
+//    }
+//}
+//
+//string path = "C:/Новая папка/Lesson/Databases/DZ";
+//CatalogInfo(path);
 
-///// Что бы пользоваться нужной деректорией.
+// Задача в игровом формате.
+// Я её не понял.
+//void Tovers(string with = "1", string on = "3", string some = "2", int count = 3)
+//{
+//    if(count > 1)
+//    {
+//        Tovers (with, some, on, count - 1);
+//    }
+//    Console.WriteLine($"{with} >> {on}");
+//    if(count > 1)
+//    {
+//        Tovers (some, on, with, count - 1);
+//    }
+//}
+//
+//Tovers();
 
-void CatalogInfo(string path, string indent = "")
-{
-    DirectoryInfo catalog = new DirectoryInfo(path);
+// Задача - Обход разных структур в уравнение.
+// ((4-2) * (1 + 3)) / 10
+// Бинарное дерево:
+//                        (/)1
+//            (*) 2           (10)3
+//      (-)4         (+)5
+//    (4)8  (2)9   (1)10   (3)11
+// Теперь всё нужно пронумировать определённым образом.
+//  (/) - 1
+//  (*) - 2
+//  (10) - 3
+//  (-) - 4
+//  (+) - 5
+//  (4) - 8
+//  (2) - 9
+//  (1) - 10
+//  (3) - 11
+//  Цифры 4 и 2 пронумерованы 8 и 9 потому, что если бы под цифрой 10 было ещё одно выражение то
+//  оно бы было пронумеровано как 6 и 7, по этому 6 и 7 мы не используем.
+//  А так же процесс нумерования происходит слева направо.
+//  Как пример буквенно будет выглядеть так:
+//          (Ai)
+//     (A2i)  (A2i+1)
 
-    DirectoryInfo[] catalog = catalog.GetDirectories();
-    for (int i = 0; i < catalog.Length; i++)
-    {
-        Console.WriteLine($"{indent}{catalog[i].Name}");
-        CatalogInfo(catalogs[i].FullName, indent + " ");
-    }
+// Решение мне не понятно.
+//
+//string emp = String.Empty;
+//string[] tree = { emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3" };
+////                  0    1    2     3    4    5    6    7    8    9    10   11
+//// где emp - указывает на цифру который мы не воспользовались.
+//
+//void InOrderTraversal (int pos = 1)
+//{
+//    if(pos < tree.Length)
+//    {
+//        int left = 2 * pos;
+//        int rigth = 2 * pos + 1;
+//        if(left < tree.Length && !String.IsNullOrEmpty(tree[left]))
+//        {
+//            InOrderTraversal(left);
+//        }
+//        Console.Write(tree[pos]);
+//        if(rigth < tree.Length && !String.IsNullOrEmpty(tree[rigth]))
+//        {
+//            InOrderTraversal(rigth);
+//        }
+//    }
+//}
+//
+//InOrderTraversal();
+//Console.WriteLine();
 
-    FileInfo[] files = catalog.GetFiles();
-
-    for (int i = 0; i < files.Length; i++)
-    {
-        Console.WriteLine($"{indent}{files[i]}.Name");
-    }
-}
-
-string path = "";
-CatalogInfo(path);
+// Про деревья - https://ru.wikipedia.org/wiki/%D0%94%D0%B2%D0%BE%D0%B8%D1%87%D0%BD%D0%BE%D0%B5_%D0%B4%D0%B5%D1%80%D0%B5%D0%B2%D0%BE
+// Про обходы - https://ru.wikipedia.org/wiki/%D0%9E%D0%B1%D1%85%D0%BE%D0%B4_%D0%B4%D0%B5%D1%80%D0%B5%D0%B2%D0%B0
+// По HTML можно ходить благодаря этим деревьям.
+// 
+// 
