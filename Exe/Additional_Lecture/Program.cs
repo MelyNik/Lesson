60aa26d1f5f97befec6c4dedbea93fa87ed320c2
Nikita <number_nik@mail.ru>
2022-09-20 17:23:00 +0300

    Дополнительная лекция C#

---
+++ Exe/Additional_Lecture/Program.cs
@@ -0,0 +1,30 @@
+﻿
+//// System.Console.WriteLine($"Hello World");
+//
+//using System; // Берём пространство имён из System.
+//using System.Linq;     // Всё это пространство имён.
+//using System.Text;     // Всё это пространство имён. (текст)
+//using System.Net.Http; // Всё это пространство имён. (интернет)
+//
+//namespace ConsoleVS // Тоже пространство имён.
+//{
+//    class Program // Без класс, методы не могут быть.
+//    {
+//        static public void Main(string[] args) // Функция
+//        {
+//            if (args.Length >= 1)
+//            {
+//                if (args[0] == "")
+//                {
+//                    Console.WriteLine($"Hello {args[0].Replace("-", "")}!");
+//                }
+//            }
+//            Console.ReadKey(); // Указыват на то, что достаточно нажать любую кнопку.
+//        }
+//    }
+//}
+//// С начало наш код превращается в код возможный открыть всеми, а после этого
+//// машиной превращается в код понятный для компьютера.
+//
+
+
