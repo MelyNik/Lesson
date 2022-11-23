3a8ef466cf77a5e5ffdd28f0ec9652fe17bc2177
Nikita <number_nik@mail.ru>
2022-11-23 17:47:02 +0300

    Лекция 1 API

---
+++ Java_S_L/Lectures/Lesson_1/Project.java
@@ -0,0 +1,12 @@
+package Java_S_L.Lectures.Lesson_1;
+// Начало программы это class и main,
+/**
+ * 1_1
+ */
+public class Project {
+    public static void main(String[] args) {
+        System.out.println("bye world"); // Лучше sysout
+        // java Project.java
+    }
+    
+}
