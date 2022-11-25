7e2d3d94c8d0aa71e98d40cb8593cbdf646098a9
Nikita <number_nik@mail.ru>
2022-11-25 17:44:15 +0300

    Решение задач Семинар 1 java

--- Java_S_L/Seminar/Seminar_1/proj_3.java
+++ Java_S_L/Seminar/Seminar_1/proj_3.java
@@ -1,5 +1,19 @@
 package Java_S_L.Seminar.Seminar_1;
 
+import java.time.LocalTime;
+import java.util.Scanner;
+
 public class proj_3 {
-    pu
+    public static void main(String[] args) {
+        System.out.printf("Введите имя: ");
+        Scanner qwe = new Scanner(System.in);
+        String name = qwe.nextLine();
+        LocalTime nowtime = LocalTime.now(); // Вывод только времени.
+        
+        if ()
+            {
+                
+            }
+
+    }
 }
